6850fb18e9525686d8235165ab6f72d6d8bf2b1b
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-08-14 00:13:26 +0500

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,84 +28,84 @@ $("#filter-toggler").click(function () {
 });
 
 $(".list-tabs button").click(function (e) {
+  $(".list-tabs button").removeClass("active");
+  $(e.target).addClass("active");
   $(".data-tables table").css("display", "none");
   $($(this).attr("data-target")).css("display", "table");
 });
 
-let dailyNavMarkup = "";
+// let dailyNavMarkup = "";
 
-data1.forEach((el, idx) => {
-  const { name, type, series, date, assetClass, navPerUnit, navChange } = el;
+// data1.forEach((el, idx) => {
+//   const { name, type, series, date, assetClass, navPerUnit, navChange } = el;
 
-  dailyNavMarkup += `  
-            <tr>
-              <td>${name}</td>
-              <td>
-                <div className="tag blue">${type}</div>
-              </td>
-              <td>
-                <div className="tag">${series}</div>
-              </td>
-              <td>${date}</td>
-              <td>${assetClass}</td>
-              <td>${navPerUnit}</td>
-              <td>${navChange}</td>
-            </tr>
-  `;
-});
+//   dailyNavMarkup += `
+//             <tr>
+//               <td style="min-width: 150px">${name}</td>
+//               <td>
+//                 <div className="tag blue">${type}</div>
+//               </td>
+//               <td>
+//                 <div className="tag">${series}</div>
+//               </td>
+//               <td>${date}</td>
+//               <td>${assetClass}</td>
+//               <td>${navPerUnit}</td>
+//               <td>${navChange}</td>
+//             </tr>
+//   `;
+// });
 
-$("#daily-nav tbody").html(dailyNavMarkup);
+// $("#daily-nav tbody").html(dailyNavMarkup);
 
 $("#daily-nav").dataTable({ paging: false, search: false });
 
-let performanceMarkup = "";
+// let performanceMarkup = "";
 
-data2.forEach((el, idx) => {
-  const { name, date, m1, m3, m6, ytd, y1, y3, y5, y10, si, siDate } = el;
+// data2.forEach((el, idx) => {
+//   const { name, date, m1, m3, m6, ytd, y1, y3, y5, y10, si, siDate } = el;
 
-  performanceMarkup += `
-            <tr>
-              <td>${name}</td>
-              <td>${date}</td>
-              <td>${m1}</td>
-              <td>${m3}</td>
-              <td>${m6}</td>
-              <td>${ytd}</td>
-              <td>${y1}</td>
-              <td>${y3}</td>
-              <td>${y5}</td>
-              <td>${y10}</td>
-              <td>${si}</td>
-              <td>${siDate}</td>
-            </tr>
-  `;
-});
+//   performanceMarkup += `
+//             <tr>
+//               <td style="min-width: 150px">${name}</td>
+//               <td>${date}</td>
+//               <td>${m1}</td>
+//               <td>${m3}</td>
+//               <td>${m6}</td>
+//               <td>${ytd}</td>
+//               <td>${y1}</td>
+//               <td>${y3}</td>
+//               <td>${y5}</td>
+//               <td>${y10}</td>
+//               <td>${si}</td>
+//               <td>${siDate}</td>
+//             </tr>
+//   `;
+// });
 
-$("#performance-table tbody").html(performanceMarkup);
+// $("#performance-table tbody").html(performanceMarkup);
 
 $("#performance-table").dataTable({ paging: false, search: false });
 
-let distributionsMarkup = "";
+// let distributionsMarkup = "";
 
-data3.forEach((el, idx) => {
-  const { name, series, freq, date, amount, ytd } = el;
+// data3.forEach((el, idx) => {
+//   const { name, series, freq, date, amount, ytd } = el;
 
-  distributionsMarkup += `  
-            <tr>
-              <td>${name}</td>
-              <td>
-                <div className="tag">${series}</div>
-              </td>
-              <td>${freq}</td>
-              <td>${date}</td>
-              <td>${amount}</td>
-              <td>${ytd}</td>
-            </tr>
-  `;
-});
+//   distributionsMarkup += `
+//             <tr>
+//               <td style="min-width: 150px">${name}</td>
+//               <td>
+//                 <div className="tag">${series}</div>
+//               </td>
+//               <td>${freq}</td>
+//               <td>${date}</td>
+//               <td>${amount}</td>
+//               <td>${ytd}</td>
+//             </tr>
+//   `;
+// });
 
-$("#distributions-table tbody").html(distributionsMarkup);
+// $("#distributions-table tbody").html(distributionsMarkup);
 
 $("#distributions-table").dataTable({ paging: false, search: false });
-
-$;
