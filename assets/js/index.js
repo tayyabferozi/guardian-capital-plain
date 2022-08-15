cd0b361100f7681bd5dd86a3948757e18e8fbfd1
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-08-15 18:38:47 +0500

    fixes filter issues

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// window.addEventListener("load", () => {
 const setScrolled = () => {
   if (window.pageYOffset > 10) {
     $("#navbar").addClass("scrolled");
@@ -29,83 +30,354 @@ $("#filter-toggler").click(function () {
 
 $(".list-tabs button").click(function (e) {
   $(".list-tabs button").removeClass("active");
-  $(e.target).addClass("active");
+  $(`[data-target="${[$(this).attr("data-target")]}"]`).addClass("active");
+  console.log($($(this).attr("data-target")));
   $(".data-tables table").css("display", "none");
   $($(this).attr("data-target")).css("display", "table");
 });
 
-// let dailyNavMarkup = "";
+const dailyNavTable1 = $(".daily-nav-table").eq(0).DataTable({ paging: false });
+const dailyNavTable2 = $(".daily-nav-table").eq(1).DataTable({ paging: false });
 
-// data1.forEach((el, idx) => {
-//   const { name, type, series, date, assetClass, navPerUnit, navChange } = el;
+const performanceTable1 = $(".performance-table")
+  .eq(0)
+  .DataTable({ paging: false });
+const performanceTable2 = $(".performance-table")
+  .eq(1)
+  .DataTable({ paging: false });
 
-//   dailyNavMarkup += `
-//             <tr>
-//               <td style="min-width: 150px">${name}</td>
-//               <td>
-//                 <div className="tag blue">${type}</div>
-//               </td>
-//               <td>
-//                 <div className="tag">${series}</div>
-//               </td>
-//               <td>${date}</td>
-//               <td>${assetClass}</td>
-//               <td>${navPerUnit}</td>
-//               <td>${navChange}</td>
-//             </tr>
-//   `;
+const distributionsTable1 = $(".distributions-table").eq(0).DataTable({
+  paging: false,
+});
+const distributionsTable2 = $(".distributions-table").eq(1).DataTable({
+  paging: false,
+});
+
+$("#search-funds").keyup(function () {
+  const $thisVal = $(this).val();
+  dailyNavTable1.search($thisVal).draw();
+  dailyNavTable2.search($thisVal).draw();
+  performanceTable1.search($thisVal).draw();
+  performanceTable2.search($thisVal).draw();
+  distributionsTable1.search($thisVal).draw();
+  distributionsTable2.search($thisVal).draw();
+});
+
+$(".tabs .tab").click(function (e) {
+  const $this = $(e.target);
+
+  $(".tabs .tab").removeClass("active");
+  $(".data-tables-with-filters").css("display", "none");
+  $this.addClass("active");
+  $($this.attr("data-target")).css("display", "block");
+});
+
+$(".filter-box-items > button").click(function () {
+  const $this = $(this);
+  $this.toggleClass("active");
+});
+
+// $(".tabs .tab").click();
+
+$(".fund-series.filter-box-items > button").click(function () {
+  let searchQuery = "";
+  const activeBtns = $(".fund-series.filter-box-items > button.active");
+
+  if (activeBtns.length > 0) {
+    activeBtns.each(function (idx, el) {
+      const text = $(el).text().trim();
+      searchQuery += "|" + text;
+      // searchQuery += "|\\b" + text + "\\b";
+    });
+    searchQuery = searchQuery.substring(1);
+    // searchQuery = "(" + searchQuery + ")";
+    searchQuery = "(^" + searchQuery + "$)";
+
+    console.log(searchQuery);
+
+    dailyNavTable1.columns([2]).search(`${searchQuery}`, true, false).draw();
+    distributionsTable1
+      .columns([1])
+      .search(`${searchQuery}`, true, false)
+      .draw();
+    performanceTable1
+      .columns([14])
+      .search(`${searchQuery}`, true, false)
+      .draw();
+  } else {
+    dailyNavTable1.columns([2]).search("").draw();
+    distributionsTable1.columns([1]).search("").draw();
+    performanceTable1.columns([14]).search("").draw();
+  }
+});
+
+// $(".etf-hedged.filter-box-items > button").click(function () {
+//   let searchQuery = "";
+//   const activeBtns = $(".etf-hedged.filter-box-items > button.active");
+
+//   if (activeBtns.length > 0) {
+//     activeBtns.each(function (idx, el) {
+//       const text = $(el).text().trim();
+//       searchQuery += "|" + text;
+//       // searchQuery += "|\\b" + text + "\\b";
+//     });
+//     searchQuery = searchQuery.substring(1);
+//     // searchQuery = "(" + searchQuery + ")";
+//     searchQuery = "(^" + searchQuery + "$)";
+
+//     console.log(searchQuery);
+
+//     dailyNavTable2.columns([7]).search(`${searchQuery}`, true, false).draw();
+//     performanceTable2.columns([4]).search(`${searchQuery}`, true, false).draw();
+//     distributionsTable2
+//       .columns([7])
+//       .search(`${searchQuery}`, true, false)
+//       .draw();
+//   } else {
+//     dailyNavTable2.columns([7]).search("").draw();
+//     performanceTable2.columns([4]).search("").draw();
+//     distributionsTable2.columns([7]).search("").draw();
+//   }
 // });
 
-// $("#daily-nav tbody").html(dailyNavMarkup);
+$(".journey-list.filter-box-items > button").click(function () {
+  let searchQuery = "";
+  const activeBtns = $(".journey-list.filter-box-items > button.active");
 
-$("#daily-nav").dataTable({ paging: false, search: false });
+  if (activeBtns.length > 0) {
+    activeBtns.each(function (idx, el) {
+      const text = $(el).text().trim();
+      searchQuery += "|" + text[0];
+    });
+    searchQuery = searchQuery.substring(1);
+    searchQuery = "(^" + searchQuery + "$)";
 
-// let performanceMarkup = "";
+    console.log(searchQuery);
 
-// data2.forEach((el, idx) => {
-//   const { name, date, m1, m3, m6, ytd, y1, y3, y5, y10, si, siDate } = el;
+    dailyNavTable1.columns([1]).search(`${searchQuery}`, true, false).draw();
+    performanceTable1
+      .columns([13])
+      .search(`${searchQuery}`, true, false)
+      .draw();
+    distributionsTable1
+      .columns([7])
+      .search(`${searchQuery}`, true, false)
+      .draw();
+  } else {
+    dailyNavTable1.columns([1]).search("").draw();
+    performanceTable1.columns([13]).search("").draw();
+    distributionsTable1.columns([7]).search("").draw();
+  }
+});
 
-//   performanceMarkup += `
-//             <tr>
-//               <td style="min-width: 150px">${name}</td>
-//               <td>${date}</td>
-//               <td>${m1}</td>
-//               <td>${m3}</td>
-//               <td>${m6}</td>
-//               <td>${ytd}</td>
-//               <td>${y1}</td>
-//               <td>${y3}</td>
-//               <td>${y5}</td>
-//               <td>${y10}</td>
-//               <td>${si}</td>
-//               <td>${siDate}</td>
-//             </tr>
-//   `;
+$(".etf-journey-list.filter-box-items > button").click(function () {
+  let searchQuery = "";
+  const activeBtns = $(".etf-journey-list.filter-box-items > button.active");
+
+  if (activeBtns.length > 0) {
+    activeBtns.each(function (idx, el) {
+      const text = $(el).text().trim();
+      searchQuery += "|" + text[0];
+    });
+    searchQuery = searchQuery.substring(1);
+    searchQuery = "(^" + searchQuery + "$)";
+
+    console.log(searchQuery);
+
+    dailyNavTable2.columns([1]).search(`${searchQuery}`, true, false).draw();
+    performanceTable2.columns([3]).search(`${searchQuery}`, true, false).draw();
+    distributionsTable2
+      .columns([2])
+      .search(`${searchQuery}`, true, false)
+      .draw();
+  } else {
+    dailyNavTable2.columns([1]).search("").draw();
+    performanceTable2.columns([3]).search("").draw();
+    distributionsTable2.columns([2]).search("").draw();
+  }
+});
+
+$(".assets-list input").change(function () {
+  let searchQuery = "";
+  const activeInputs = $(".assets-list input:checked");
+
+  if (activeInputs.length > 0) {
+    activeInputs.each(function (idx, el) {
+      const text = $(el).attr("id").replace(/-/g, " ").replace(/_/g, ".");
+      searchQuery += "|" + text;
+    });
+    searchQuery = searchQuery.substring(1);
+    searchQuery = "(^" + searchQuery + "$)";
+
+    console.log(searchQuery);
+
+    dailyNavTable1.columns([4]).search(`${searchQuery}`, true, false).draw();
+    performanceTable1
+      .columns([12])
+      .search(`${searchQuery}`, true, false)
+      .draw();
+    distributionsTable1
+      .columns([6])
+      .search(`${searchQuery}`, true, false)
+      .draw();
+  } else {
+    dailyNavTable1.columns([4]).search("").draw();
+    performanceTable1.columns([12]).search("").draw();
+    distributionsTable1.columns([6]).search("").draw();
+  }
+});
+
+$(".etf-assets-list input").change(function () {
+  let searchQuery = "";
+  const activeInputs = $(".etf-assets-list input:checked");
+
+  console.log(activeInputs);
+
+  if (activeInputs.length > 0) {
+    activeInputs.each(function (idx, el) {
+      const text = $(el)
+        .attr("id")
+        .replace(/etf-/g, "")
+        .replace(/-/g, " ")
+        .replace(/_/g, ".");
+      searchQuery += "|" + text;
+    });
+    searchQuery = searchQuery.substring(1);
+    searchQuery = "(^" + searchQuery + "$)";
+
+    console.log(searchQuery);
+
+    dailyNavTable2.columns([4]).search(`${searchQuery}`, true, false).draw();
+    performanceTable2.columns([2]).search(`${searchQuery}`, true, false).draw();
+    distributionsTable2
+      .columns([1])
+      .search(`${searchQuery}`, true, false)
+      .draw();
+  } else {
+    dailyNavTable2.columns([4]).search("").draw();
+    performanceTable2.columns([2]).search("").draw();
+    distributionsTable2.columns([1]).search("").draw();
+  }
+});
 // });
 
-// $("#performance-table tbody").html(performanceMarkup);
+$(".fund-group-select-all").click(function () {
+  const $this = $(this);
 
-$("#performance-table").dataTable({ paging: false, search: false });
+  $this
+    .parents(".card")
+    .find(".filter-box-item")
+    .each(function (idx, el) {
+      const $this2 = $(el);
 
-// let distributionsMarkup = "";
+      if (!$this2.hasClass("active")) {
+        $this2.click();
+      }
+    });
+});
 
-// data3.forEach((el, idx) => {
-//   const { name, series, freq, date, amount, ytd } = el;
+$(".fund-group-clear-all").click(function () {
+  const $this = $(this);
 
-//   distributionsMarkup += `
-//             <tr>
-//               <td style="min-width: 150px">${name}</td>
-//               <td>
-//                 <div className="tag">${series}</div>
-//               </td>
-//               <td>${freq}</td>
-//               <td>${date}</td>
-//               <td>${amount}</td>
-//               <td>${ytd}</td>
-//             </tr>
-//   `;
-// });
+  $this
+    .parents(".card")
+    .find(".filter-box-item")
+    .each(function (idx, el) {
+      const $this2 = $(el);
 
-// $("#distributions-table tbody").html(distributionsMarkup);
+      if ($this2.hasClass("active")) {
+        $this2.click();
+      }
+    });
+});
 
-$("#distributions-table").dataTable({ paging: false, search: false });
+$(".asset-class-select-all").click(function () {
+  const $this = $(this);
+
+  $this
+    .parents(".card")
+    .find("input[type='checkbox']")
+    .each(function (idx, el) {
+      const $this2 = $(el);
+
+      if (!$this2.is(":checked")) {
+        $this2.click();
+      }
+    });
+});
+
+$(".asset-class-clear-all").click(function () {
+  const $this = $(this);
+
+  $this
+    .parents(".card")
+    .find("input[type='checkbox']")
+    .each(function (idx, el) {
+      const $this2 = $(el);
+      console.log($this.is(":checked"));
+
+      if ($this2.is(":checked")) {
+        $this2.click();
+      }
+    });
+});
+
+$(".fund-series-select-all").click(function () {
+  const $this = $(this);
+
+  $this
+    .parents(".card")
+    .find(".filter-box-item")
+    .each(function (idx, el) {
+      const $this2 = $(el);
+
+      if (!$this2.hasClass("active")) {
+        $this2.click();
+      }
+    });
+});
+
+$(".fund-series-clear-all").click(function () {
+  const $this = $(this);
+
+  $this
+    .parents(".card")
+    .find(".filter-box-item")
+    .each(function (idx, el) {
+      const $this2 = $(el);
+
+      if ($this2.hasClass("active")) {
+        $this2.click();
+      }
+    });
+});
+
+$(".hedged-select-all").click(function () {
+  const $this = $(this);
+
+  $this
+    .parents(".card")
+    .find(".filter-box-item")
+    .each(function (idx, el) {
+      const $this2 = $(el);
+
+      if (!$this2.hasClass("active")) {
+        $this2.click();
+      }
+    });
+});
+
+$(".hedged-clear-all").click(function () {
+  const $this = $(this);
+
+  $this
+    .parents(".card")
+    .find(".filter-box-item")
+    .each(function (idx, el) {
+      const $this2 = $(el);
+
+      if ($this2.hasClass("active")) {
+        $this2.click();
+      }
+    });
+});
